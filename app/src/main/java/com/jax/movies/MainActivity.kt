538445e474c016b3ba327abea9b1ae44556d71f0
zhanibekishimss <230103150@sdu.edu.kz>
2024-10-14 17:48:19 +0500

    Initial commit

---
+++ app/src/main/java/com/jax/movies/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jax.movies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.jax.movies.ui.theme.MoviesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MoviesTheme {
+
+            }
+        }
+    }
+}
