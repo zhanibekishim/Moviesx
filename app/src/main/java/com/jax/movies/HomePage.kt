b8fea38a022c6cae5565b42eff32f076d6cd8331
akbooo <subb04a@gmail.com>
2024-10-15 21:51:45 +0500

    a

--- app/src/main/java/com/jax/movies/HomePage.kt
+++ app/src/main/java/com/jax/movies/HomePage.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,54 +34,24 @@ import com.jax.movies.data.allMovies
 fun HomePage(){
     val scrollState = rememberScrollState()
     Column(modifier = Modifier.fillMaxSize()
-        .padding(10.dp)
+        .padding(18.dp)
         .verticalScroll(state = scrollState),
         verticalArrangement = Arrangement.spacedBy(24.dp)) {
         Spacer(Modifier.height(50.dp))
         Image(painter = painterResource(R.drawable.vector),
             contentDescription = null)
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(allMovies.premieres){
-                item -> MovieItem(item)
-            }
-        }
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-            items(allMovies.popular){
-                item -> MovieItem(item)
-            }
-        }
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(allMovies.militants){
-                item -> MovieItem(item)
-            }
-        }
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(allMovies.dramaOfFrance) { item ->
-                MovieItem(item)
-            }
-        }
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(allMovies.top){
-                item -> MovieItem(item)
-            }
-        }
-        LazyRow(modifier = Modifier.padding(top = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            items(allMovies.show){
-                item -> MovieItem(item)
-            }
-        }
+        LazyRowItem(allMovies.premieres, "Премьера")
+        LazyRowItem(allMovies.popular, "Популярное")
+        LazyRowItem(allMovies.militants, "Боевики США")
+        LazyRowItem(allMovies.dramaOfFrance, "Драма Франции")
+        LazyRowItem(allMovies.top, "Топ-5")
+        LazyRowItem(allMovies.show, "Сериалы")
     }
 }
 
 @Composable
 fun MovieItem(movie: Movie) {
-    Column(modifier = Modifier) {
+    Column {
         Image(painter = painterResource(R.drawable.zamena),
             contentDescription = "image of cinema",
             modifier = Modifier.height(156.dp)
@@ -94,6 +65,25 @@ fun MovieItem(movie: Movie) {
             text = movie.genre,
             fontSize = 12.sp
         )
+    }
+}
+
+@Composable
+fun LazyRowItem(items: List<Movie>,
+                type: String){
+    Column(modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(6.dp)) {
+        Text(text = type,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold)
+        LazyRow(
+            modifier = Modifier.padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(items) { item ->
+                MovieItem(item)
+            }
+        }
     }
 }
 
