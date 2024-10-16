6a28076a397232c80009bda692a2aa4fc5690131
Romanid04 <118803238+Romanid04@users.noreply.github.com>
2024-10-16 11:50:55 +0500

    2) Check update in MainActivity

--- app/src/main/java/com/jax/movies/MainActivity.kt
+++ app/src/main/java/com/jax/movies/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import com.jax.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +15,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MoviesTheme {
                 //HomePage()
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    OnBoardingScreen(onFinish = {
-
-                        // Действие, когда пользователь завершает экран onboarding
-                        // Например, переход к главному экрану приложения
-                    })
-                }
+                OnBoardingScreen(onFinish = {
+                    // Действие, когда пользователь завершает экран onboarding
+                    // Например, переход к главному экрану приложения
+                })
             }
         }
     }
 }
+
