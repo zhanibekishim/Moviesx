e0854154e2dd4e66c80da13e1ed874d76900e417
zhanibekishimss <230103150@sdu.edu.kz>
2024-12-13 02:27:02 +0500

    fixed bags in search and profile screen

--- app/src/main/java/com/jax/movies/presentation/components/ActorItem.kt
+++ app/src/main/java/com/jax/movies/presentation/components/ActorItem.kt
@@ -1,6 +1,5 @@
 package com.jax.movies.presentation.components
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
